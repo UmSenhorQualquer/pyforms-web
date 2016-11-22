a0210f529d7a2974ad4596eadf4979879542b8bc
Ricardo Ribeiro <ricardo.ribeiro@neuro.fchampalimaud.org>
2016-11-22 23:11:50 +0000

    .

--- pyforms_web/web/django/Applications.py
+++ pyforms_web/web/django/Applications.py
@@ -1,4 +1,4 @@
-import datetime, json
+import datetime, json, dill, os
 from pysettings import conf
 from crequest.middleware import CrequestMiddleware
 
@@ -31,6 +31,7 @@ class UserRunningApps(object):
 	def add_app(self, app):
 		if app.uid in self._apps.keys(): return
 		self._apps[app.uid] = [datetime.datetime.now(), app]
+
 		
 	def remove_app(self, app_id):
 		item = self._apps.get(app_id, None)
@@ -64,6 +65,12 @@ class ApplicationsLoader:
 
 	@staticmethod
 	def create_instance(request, modulename, app_data=None):
+		if os.path.isfile('oppened-apps.dat'):
+			with open('oppened-apps.dat', 'r') as f:
+				self._opened_apps = dill.loads(f)
+		else:
+			self._opened_apps = {}
+		
 		updated_apps = request.updated_apps = Apps2Update()
 
 		# check if the module was already imported, if not import it.
@@ -76,11 +83,21 @@ class ApplicationsLoader:
 		app = moduleclass()
 
 		for m in updated_apps.applications: m.commit()
+
+		with open('oppened-apps.dat', 'w') as f:
+			dill.dump(self._opened_apps, f)
+		
 
 		return app
 
 	@staticmethod
 	def run_instance(request, modulename, app_data=None):
+		if os.path.isfile('oppened-apps.dat'):
+			with open('oppened-apps.dat', 'r') as f:
+				self._opened_apps = dill.loads(f)
+		else:
+			self._opened_apps = {}
+
 		updated_apps = request.updated_apps = Apps2Update()
 
 		# check if the module was already imported, if not import it.
@@ -96,16 +113,34 @@ class ApplicationsLoader:
 		data = [r.serializeForm() for r in results if r.is_new_app]
 		for m in results: m.commit()
 
+		with open('oppened-apps.dat', 'w') as f:
+			dill.dump(self._opened_apps, f)
+
 		return data
 
 	@staticmethod
 	def remove_instance(request, application_id):
+		if os.path.isfile('oppened-apps.dat'):
+			with open('oppened-apps.dat', 'r') as f:
+				self._opened_apps = dill.loads(f)
+		else:
+			self._opened_apps = {}
+
 		user_apps = ApplicationsLoader._opened_apps.get(request.user, None)
 		if user_apps is None: return None
 		user_apps.remove_app(application_id)
+
+		with open('oppened-apps.dat', 'w') as f:
+			dill.dump(self._opened_apps, f)
 		
 	@staticmethod
 	def get_instance(request, application_id, app_data=None):
+		if os.path.isfile('oppened-apps.dat'):
+			with open('oppened-apps.dat', 'r') as f:
+				self._opened_apps = dill.loads(f)
+		else:
+			self._opened_apps = {}
+
 		updated_apps = request.updated_apps = Apps2Update()
 
 		user_apps = ApplicationsLoader._opened_apps.get(request.user, None)
@@ -116,11 +151,20 @@ class ApplicationsLoader:
 		if app_data is not None: app.loadSerializedForm(app_data)
 
 		for m in updated_apps.applications: m.commit()
+
+		with open('oppened-apps.dat', 'w') as f:
+			dill.dump(self._opened_apps, f)
 		return app
 
 
 	@staticmethod
 	def update_instance(request, application_id, app_data=None):
+		if os.path.isfile('oppened-apps.dat'):
+			with open('oppened-apps.dat', 'r') as f:
+				self._opened_apps = dill.loads(f)
+		else:
+			self._opened_apps = {}
+
 		updated_apps = request.updated_apps = Apps2Update()
 
 		user_apps = ApplicationsLoader._opened_apps.get(request.user, None)
@@ -133,10 +177,19 @@ class ApplicationsLoader:
 		data = [r.serializeForm() for r in results if r.is_new_app]
 		for m in results: m.commit()
 
+		with open('oppened-apps.dat', 'w') as f:
+			dill.dump(self._opened_apps, f)
+
 		return data
 
 	@staticmethod
 	def createInstance(modulename, user, data=None, app_id=None ):
+		if os.path.isfile('oppened-apps.dat'):
+			with open('oppened-apps.dat', 'r') as f:
+				self._opened_apps = dill.loads(f)
+		else:
+			self._opened_apps = {}
+
 		if data is not None or app_id is not None:
 			if app_id is None: app_id = data['uid']
 			app  = ApplicationsLoader._opened_apps[user].get_app(app_id)
@@ -151,15 +204,26 @@ class ApplicationsLoader:
 			app = moduleclass()
 			app.application = modulename
 
+		with open('oppened-apps.dat', 'w') as f:
+			dill.dump(self._opened_apps, f)
 			
 		return app
 
 	@staticmethod
 	def add_app(user, app):
+		if os.path.isfile('oppened-apps.dat'):
+			with open('oppened-apps.dat', 'r') as f:
+				self._opened_apps = dill.loads(f)
+		else:
+			self._opened_apps = {}
+
 		# register the application globaly
 		if user not in ApplicationsLoader._opened_apps.keys():
 			ApplicationsLoader._opened_apps[user] = UserRunningApps()
 		ApplicationsLoader._opened_apps[user].add_app(app)
 		CrequestMiddleware.get_request().updated_apps.add_top(app)
+
+		with open('oppened-apps.dat', 'w') as f:
+			dill.dump(self._opened_apps, f)
 		
 	
