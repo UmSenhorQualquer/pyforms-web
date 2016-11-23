64a58b26b91ddf94f5f22928a001fef64e544e75
Ricardo Ribeiro <ricardo.ribeiro@neuro.fchampalimaud.org>
2016-11-23 12:15:53 +0000

    .

--- pyforms_web/web/django/Applications.py
+++ pyforms_web/web/django/Applications.py
@@ -64,6 +64,20 @@ class ApplicationsLoader:
 	_opened_apps = {}
 
 	@staticmethod
+	def createInstance(modulename):
+
+		if modulename not in ApplicationsLoader._storage:
+			modules = modulename.split('.')
+			moduleclass = __import__( '.'.join(modules[:-1]) , fromlist=[modules[-1]] )
+			ApplicationsLoader._storage[modulename] = getattr(moduleclass, modules[-1])
+		
+		moduleclass = ApplicationsLoader._storage[modulename]
+
+		obj = moduleclass()
+		obj.modulename = modulename
+		return obj
+
+	@staticmethod
 	def create_instance(request, modulename, app_data=None):
 		if os.path.isfile('/var/www/orquestra-server/oppened-apps.dat'):
 			with open('/var/www/orquestra-server/oppened-apps.dat', 'r') as f:
