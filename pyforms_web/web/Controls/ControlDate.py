4f9ea0bfaa90d10003c0fcd44b72bb7a426608b2
Ricardo Ribeiro <ricardo.ribeiro@neuro.fchampalimaud.org>
2017-03-15 17:02:29 +0000

    .

--- pyforms_web/web/Controls/ControlDate.py
+++ pyforms_web/web/Controls/ControlDate.py
@@ -11,7 +11,7 @@ class ControlDate(ControlBase):
 
 	@property
 	def value(self): 
-		if isinstance(self._value, str):
+		if isinstance(self._value, (str,unicode) ):
 			return self._value
 		elif self._value==None:
 			return ''
