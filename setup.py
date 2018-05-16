2b45b9aecb91ee0bf6c0db3460fa55c056a1af4b
Hugo Cachitas <hugo.cachitas@research.fchampalimaud.org>
2018-05-16 14:58:39 +0100

    Fix identation and add requirements

--- setup.py
+++ setup.py
@@ -20,25 +20,33 @@ def find_files(package_name,directory, pattern):
                 filename = os.path.join(root[len(package_name)+1:], basename)
                 yield filename
 
-setup(
 
-	name				= 'PyForms-Web',
-	version 			= '0.1.6',
-	description 		= """
-		Pyforms is a Python 2.7 and 3.0 framework to develop GUI application,
-		which promotes modular software design and code reusability with minimal effort.
-	""",
-	author  			 = 'Ricardo Ribeiro',
-	author_email		 = 'ricardojvr@gmail.com',
-	license 			 = 'MIT',
-	url 				 = 'https://github.com/UmSenhorQualquer/pyforms',
-	include_package_data = True,
-	packages=find_packages(),
-	package_data={
-		'pyforms_web':
-		list(
-			find_files('pyforms_web','web/static/', '*.*')
-		)
-	},
-	install_requires=[],
+setup(
+    name                = 'PyForms-Web',
+    version             = '0.1.6',
+    description         = """
+        Pyforms is a Python 2.7 and 3.0 framework to develop GUI application,
+        which promotes modular software design and code reusability with minimal effort.
+    """,
+    author               = 'Ricardo Ribeiro',
+    author_email         = 'ricardojvr@gmail.com',
+    license              = 'MIT',
+    url                  = 'https://github.com/UmSenhorQualquer/pyforms',
+    include_package_data = True,
+    packages=find_packages(),
+    package_data={
+        'pyforms_web':
+        list(
+            find_files('pyforms_web','web/static/', '*.*')
+        )
+    },
+    install_requires=[
+        'django>2.0,<2.1',
+        'simplejson',
+        'sorl-thumbnail',
+        'dill',
+        'filelock',
+        'Pillow',
+        'python-dateutil',
+    ],
 )
