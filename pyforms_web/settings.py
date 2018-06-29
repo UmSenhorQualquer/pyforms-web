c370ff2b3ec20a11717be527f67615ebccdb1715
Ricardo Ribeiro <ricardojvr@gmail.com>
2018-06-29 14:46:38 +0100

    Update apps-cache conf

--- pyforms_web/settings.py
+++ pyforms_web/settings.py
@@ -1,6 +1,15 @@
 
 
-PYFORMS_WEB_APPS_TIMEOUT 			= 3600*48
-PYFORMS_WEB_APPS_CACHE_DIR 			= 'apps-cache'
+PYFORMS_WEB_APPS_TIMEOUT   = 3600*48
+
 
 PYFORMS_APPS = {}
+
+
+try:
+	import os
+	from django.conf import settings
+
+	PYFORMS_WEB_APPS_CACHE_DIR = os.path.join(settings.BASE_DIR,'apps-cache')
+except:
+	PYFORMS_WEB_APPS_CACHE_DIR = 'apps-cache'
