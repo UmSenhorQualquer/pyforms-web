ff0380b4d041fab2b3244b82ea400664e5447ba3
Ricardo Ribeiro <ricardojvr@gmail.com>
2018-10-16 09:06:28 +0100

    update pyforms app template

--- pyforms_web/web/management/pyforms_app_template.py
+++ pyforms_web/web/management/pyforms_app_template.py
@@ -1,34 +1,9 @@
-#from django_pyforms.model_admin.editform_admin import EditFormAdmin
-#from pyforms_web.controls.ControlQueryList import ControlQueryList
-
-from pyforms.basewidget import ModelAdmin
-from orquestra.plugins import LayoutPositions
+from confapp import conf
+from pyforms_web.widgets.django import ModelAdminWidget
 
 from {application_name}.models import {model_name}
 
-
-# class {model_name}EditFormAdmin(EditFormAdmin):
-
-#     MODEL     = {model_name}  #model to manage
-#     TITLE     = '{model_verbose_name}'  #title of the application
-    
-#     #INLINES   = []    #sub models to show in the interface
-#     #FIELDSETS = None  #formset of the edit form
-#     #READ_ONLY = []
-    
-#     #SAVE_BTN_LABEL     = '<i class="save icon"></i> Save'
-#     #CREATE_BTN_LABEL   = '<i class="plus icon"></i> Create'
-#     #CANCEL_BTN_LABEL   = '<i class="hide icon"></i> Close'
-#     #REMOVE_BTN_LABEL   = '<i class="trash outline icon"></i> Remove'
-#     #POPUP_REMOVE_TITLE = 'The next objects are going to be affected or removed'
-
-#     #HAS_CANCEL_BTN_ON_ADD  = True #Flag to show or hide the cancel button
-#     #HAS_CANCEL_BTN_ON_EDIT = True #Flag to show or hide the cancel button
-
-#    def delete_event(self):
-#        return super({model_name}EditFormAdmin,self).delete_event()
-
-class {model_name}AdminApp(ModelAdmin):
+class {model_name}AdminApp(ModelAdminWidget):
     
 
     UID   = '{application_name}-{model_name}-app'.lower()
@@ -57,14 +32,15 @@ class {model_name}AdminApp(ModelAdmin):
     #EDITFORM_CLASS = {model_name}EditFormAdmin    #edit form class
     #CONTROL_LIST   = ControlQueryList #Control to be used in to list the values
     
+    #AUTHORIZED_GROUPS   = ['superuser'] #groups with authorization to visualize the app
+    
     ########################################################
     #### ORQUESTRA CONFIGURATION ###########################
     ########################################################
-    LAYOUT_POSITION      = LayoutPositions.HOME
+    LAYOUT_POSITION      = conf.ORQUESTRA_HOME
     ORQUESTRA_MENU       = 'left{main_app}'
     ORQUESTRA_MENU_ORDER = 0
     ORQUESTRA_MENU_ICON  = 'dollar'
-    #AUTHORIZED_GROUPS   = ['superuser'] #groups with authorization to visualize the app
     ########################################################
     
     
