2b2904990d23ab6f5dc32b17d0cb19d208c12f3e
Ricardo Ribeiro <ricardojvr@gmail.com>
2019-03-05 16:47:51 +0000

    Fix local_settings import order

--- pyforms_web/__init__.py
+++ pyforms_web/__init__.py
@@ -9,11 +9,13 @@ __maintainer__  = "Ricardo Ribeiro"
 __email__       = "ricardojvr@gmail.com"
 __status__      = "Development"
 
+
+from confapp import conf;
+conf += 'pyforms_web.settings'
+
 #force the load of the local settings if exists
 try:
     import local_settings
     conf += local_settings
 except:
     pass
-
-from confapp import conf; conf += 'pyforms_web.settings'
