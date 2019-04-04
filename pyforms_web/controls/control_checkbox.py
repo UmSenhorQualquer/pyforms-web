eb779afa7242cdcdbe681502d735460aae228a31
Ricardo Ribeiro <ricardojvr@gmail.com>
2019-04-05 00:06:25 +0100

    Add checkbox_type parameter to ControlCheckBox

--- pyforms_web/controls/control_checkbox.py
+++ pyforms_web/controls/control_checkbox.py
@@ -5,8 +5,15 @@ class ControlCheckBox(ControlBase):
 
     def __init__(self, *args, **kwargs):
         if 'default' not in kwargs: kwargs['default']=False
+        self.checkbox_type = kwargs.get('checkbox_type', 'toggle')
+
         super().__init__(*args, **kwargs)
     
 
-    def init_form(self): return "new ControlCheckBox('{0}', {1})".format( self._name, simplejson.dumps(self.serialize()) )
+    def init_form(self):
+        return "new ControlCheckBox('{0}', {1})".format( self._name, simplejson.dumps(self.serialize()) )
 
+    def serialize(self):
+        res = super().serialize()
+        res['checkbox_type'] = self.checkbox_type
+        return res
