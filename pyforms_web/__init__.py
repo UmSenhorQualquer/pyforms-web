06d94247a625bbe26f58080f07d9a5bd563e41a2
Ricardo Ribeiro <ricardojvr@gmail.com>
2019-05-03 13:14:32 +0100

    Update version to 4.1.0

--- pyforms_web/__init__.py
+++ pyforms_web/__init__.py
@@ -4,7 +4,7 @@
 __author__      = "Ricardo Ribeiro"
 __credits__     = ["Ricardo Ribeiro"]
 __license__     = 'GNU GPLv3'
-__version__     = "4.0.7"
+__version__     = "4.1.0"
 __maintainer__  = "Ricardo Ribeiro"
 __email__       = "ricardojvr@gmail.com"
 __status__      = "Development"
