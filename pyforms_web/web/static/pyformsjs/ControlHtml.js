f66da5b47fb54ea597033467cd13d92005853b72
Hugo Cachitas <hugo.cachitas@research.fchampalimaud.org>
2019-08-01 15:58:40 +0100

    Remove segment from ControlHtml
    Should be added explicitly using the segment() organizer.

--- pyforms_web/web/static/pyformsjs/ControlHtml.js
+++ pyforms_web/web/static/pyformsjs/ControlHtml.js
@@ -3,7 +3,10 @@ class ControlHtml extends ControlBase{
 	////////////////////////////////////////////////////////////////////////////////
 
 	init_control(){
-		var html = "<div id='"+this.place_id()+"' class='field control ControlHtml' ><label>"+this.properties.label+"</label><div class='ui segment' id='"+this.control_id()+"' ></div></div>";
+		var html = '<div id="'+this.place_id()+'" class="field control ControlHtml">'
+		if(this.properties.label_visible) html += '<label>'+this.properties.label+'</label>';
+		html += '<div id="'+this.control_id()+'"></div>';
+
 		this.jquery_place().replaceWith(html);
 
 		if(this.properties.value)
