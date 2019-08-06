5d7d5bc7b43245f9f2ace8dd1bc96f4c35b46372
Hugo Cachitas <hugo.cachitas@research.fchampalimaud.org>
2019-08-06 10:45:52 +0100

    Set minimum Django supported to 2.1

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
         'orquestra',
         'numpy',
         'opencv-python~=3.4',
-        'django>2.2',
+        'django>2.1',
         'simplejson',
         'sorl-thumbnail',
         'dill',
