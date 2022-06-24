0ad2e5c7fee9f2f9d9d99403c78d604f80313574
Ricardo Ribeiro <ricardojvr@gmail.com>
2022-06-24 14:40:41 +0100

    Update ControlMultipleUpload

--- pyforms_web/controls/control_multipleupload.py
+++ pyforms_web/controls/control_multipleupload.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 import simplejson
 from django.conf import settings
@@ -14,6 +15,29 @@ class ControlMultipleUpload(ControlBase):
     def filepaths(self):
         for filename in self.value:
             yield os.path.join(settings.MEDIA_ROOT, filename[len(settings.MEDIA_URL):])
+
+    def move_to(self, dest_dirpath):
+        dest_path = os.path.join(settings.MEDIA_ROOT, dest_dirpath)
+        dest_files = []
+
+        for filepath in self.filepaths():
+            if not os.path.exists(filepath):
+                continue
+
+            os.makedirs(dest_path, exist_ok=True)
+            filename = os.path.basename(filepath)
+            dest_filepath = os.path.join(dest_path, filename)
+            name, ext = os.path.splitext(filename)
+            count = 0
+            while os.path.exists(dest_filepath):
+                filename = f'{name}_{count}{ext}'
+                dest_filepath = os.path.join(dest_path, filename)
+                count += 1
+
+            shutil.move(filepath, dest_filepath)
+            dest_files.append(os.path.join(settings.MEDIA_URL, dest_path, filename))
+
+        return dest_files
 
     def serialize(self):
         data = super().serialize()
